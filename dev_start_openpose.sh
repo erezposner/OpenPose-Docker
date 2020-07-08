60b8b14b404e164f8a2fa1a493b4ed81ab170eec
erezposner <erezposner@gmail.com>
2020-07-08 16:41:29 +0300

    sd

--- dev_start_openpose.sh
+++ dev_start_openpose.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 # Varibles to edit
-#LOCAL_SRC_DIR=/home/user3/repos/FaceGeometryTorch
-#LOCAL_DATA_DIR=/home/user3/repos/data
-
 LOCAL_SRC_DIR=/home/user3/repos/playing_smplifyx
 LOCAL_DATA_DIR=/mnt/datasets/DVP
-LOCAL_OP=/home/user3/repos/openpose
+
 #Build
 docker stop portainer
 docker stop openpose
@@ -23,4 +20,4 @@ if [ ! "$(docker ps -q -f name=portainer)" ]; then
     # run your container
     docker run -d -p 8001:8000 -p 9001:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 fi 
-docker run --rm -p 8091:8091 -v $LOCAL_OP:/workspace/local_op -v $LOCAL_SRC_DIR:/workspace/local_station -v $LOCAL_DATA_DIR:/workspace/local_data --name openpose --gpus all --shm-size=1g --ulimit memlock=-1 -it openpose
+docker run --rm -p 8091:8091  -v $LOCAL_SRC_DIR:/workspace/local_station -v $LOCAL_DATA_DIR:/workspace/local_data --name openpose --gpus all --shm-size=1g --ulimit memlock=-1 -it openpose 
